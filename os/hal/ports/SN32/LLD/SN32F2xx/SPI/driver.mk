55aa753f131761bde9d6ea5ba56af0419f9d560c
1Conan <me@1conan.com>
2022-10-23 21:10:05 +0800

    fix paths

--- os/hal/ports/SN32/LLD/SN32F2xx/SPI/driver.mk
+++ os/hal/ports/SN32/LLD/SN32F2xx/SPI/driver.mk
@@ -1,9 +1,9 @@
 ifeq ($(USE_SMART_BUILD),yes)
 ifneq ($(findstring HAL_USE_SPI TRUE,$(HALCONF)),)
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/SN32F2xx/SPIv1/hal_pal_v2_lld.c
+PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/SN32F2xx/SPI/hal_pal_v2_lld.c
 endif
 else
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/SN32F2xx/SPIv1/hal_spi_v2_lld.c
+PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/SN32F2xx/SPI/hal_spi_v2_lld.c
 endif
 
-PLATFORMINC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/SN32F2xx/SPIv1
+PLATFORMINC += $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/LLD/SN32F2xx/SPI
